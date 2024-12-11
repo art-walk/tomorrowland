3c5ca1c1cdeaac3d3c64276636f7a860830f815c
art-walk <art.mgwalk@gmail.com>
2024-12-12 05:03:27 +0700

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,2 +1,35 @@
-<?php print("Hello 👋, from Wasmer Edge!"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pendaftaran Siswa Baru | SMK Tomorrowland</title>
+</head>
 
+<body>
+	<header>
+		<h3>Pendaftaran Siswa Baru</h3>
+		<h1>SMK Tomorrowland</h1>
+	</header>
+	
+	<h4>Menu</h4>
+	<nav>
+		<ul>
+			<li><a href="form-daftar.php">Daftar Baru</a></li>
+			<li><a href="list-siswa.php">Pendaftar</a></li>
+		</ul>
+	</nav>
+	
+	
+	<?php if(isset($_GET['status'])): ?>
+	<p>
+		<?php
+			if($_GET['status'] == 'sukses'){
+				echo "Pendaftaran siswa baru berhasil!";
+			} else {
+				echo "Pendaftaran gagal!";
+			}
+		?>
+	</p>
+	<?php endif; ?>
+	
+	</body>
+</html>
